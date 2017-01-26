19266f9c5edcef570bfaf2bc2640c8e37ade3716
karthik-vishnu <vishnukarthi40@gmail.com>
2017-01-26 10:16:28 -0800

    Evaluate a string expression
    Evaluate a string expression based on the precedence and return the
    output in int

---
+++ src/com/java/puzzle/ExpressionEvaluation.java
@@ -0,0 +1,49 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+public class ExpressionEvaluation {
+	
+	public Stack<Integer> evaluate(String expression) {
+		Stack<Integer> values = new Stack<Integer>();
+		Stack<Character> operands = new Stack<Character>();
+		
+		char[] input = expression.toCharArray();
+		
+		for (int i = 0; i < input.length; i++) {
+			if(input[i] >= '0' && input[i] <= '9') {
+				StringBuilder builder = new StringBuilder();
+				while(i < input.length && input[i] >= '0' && input[i] <= '9') {
+					builder.append(input[i++]);
+				}
+				values.push(Integer.valueOf(builder.toString()));
+			}
+			
+			if(input[i] == '+' || input[i] == '-' || input[i] ==  '*' || input[i] == '/') {
+				if(!operands.isEmpty() && hasPrecedence(operands.peek(), input[i])) {
+					
+				} else {
+					operands.push(input[i]);
+				}
+			}
+		}
+		return values;
+	}
+	
+	public boolean hasPrecedence(Character operand1 , Character operand2) {
+		if((operand1 == '/' || operand1 == '*') && (operand2 == '+' || operand2 =='-')) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ExpressionEvaluation evaluation = new ExpressionEvaluation();
+		System.out.println(evaluation.evaluate("2+3*2"));
+	}
+}
