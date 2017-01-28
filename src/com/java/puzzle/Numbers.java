79ebf353618bc2661684bf6f9cb0adb02d93ed66
karthik-vishnu <vishnukarthi40@gmail.com>
2017-01-28 11:07:53 -0800

    Splitted numbers again combined with nearest number

--- src/com/java/puzzle/Numbers.java
+++ src/com/java/puzzle/Numbers.java
@@ -22,15 +22,43 @@ public class Numbers {
 		}
 		return result;
 	}
+	
+	public int[] combine(int[] input) {
+		double len = input.length;
+		double length = len / 2;
+		int roundOff = (int)Math.ceil(length);
+		int[] result = new int[roundOff];
+		int count = 0;
+		
+		for (int i = 0; i < input.length; i++) {
+			String firstValue = String.valueOf(input[i]);
+			++i;
+			if(i < input.length) {
+				String secondValue = String.valueOf(input[i]);
+				String combinedVal = firstValue+secondValue;
+				Integer finalVal = Integer.valueOf(combinedVal);
+				result[count++] = finalVal;
+			} else {
+				result[count] = Integer.valueOf(firstValue);
+			}
+		}
+		return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int input = 12345;
+		int input = 123456;
 		Numbers numbers = new Numbers();
 		int length = numbers.getCount(input);
 		
 		int[] result = numbers.split(length,input);
+		System.out.println("splitted numbers");
 		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+			System.out.print(result[i]+", ");
+		}
+		int[] combinedNumbers = numbers.combine(result);
+		System.out.println("Combined numbers");
+		for (int i = 0; i < combinedNumbers.length; i++) {
+			System.out.print(combinedNumbers[i]+", ");
 		}
 	}
 
